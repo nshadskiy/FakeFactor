88bbd85463a3fca427c31047691021fbede15ce2
Johannes <johannes.brandstetter@cern.ch>
2017-07-19 14:33:15 +0200

    steerAll

--- ViennaTool/steerAll.sh
+++ ViennaTool/steerAll.sh
@@ -7,16 +7,16 @@ echo $output
 echo "Compiling the framework"
 sh BuildStructure.sh
 cd ../
-#make -B
+make -B
 
-#./Preselection
-#./SRHisto
-#./CRHisto
-#./steerFF
-#./fitFakeFactors
-#./calcCorrections
-#python plotCorrections.py --channel $channel
-#./convert_inputs
-#python cpTDHaftPublic.py --destination $output --channel $channel
+./Preselection
+./SRHisto
+./CRHisto
+./steerFF
+./fitFakeFactors
+./calcCorrections
+python plotCorrections.py --channel $channel
+./convert_inputs
+python cpTDHaftPublic.py --destination $output --channel $channel
 python producePublicFakeFactors.py --input $output --channel $channel
 
