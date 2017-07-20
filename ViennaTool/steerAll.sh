7c7304d1975ee874b871c066cdeaf0e47019587c
Johannes <johannes.brandstetter@cern.ch>
2017-07-20 13:47:21 +0200

    removing tmp files

--- ViennaTool/steerAll.sh
+++ ViennaTool/steerAll.sh
@@ -7,10 +7,13 @@ echo $HOME$output
 
 sed s/user=\"whoami\"/user=\"$USER\"/g Settings.h >/tmp/Settings.h
 cp /tmp/Settings.h .
+rm /tmp/Settgins.h
 sed s/user=whoami/user=$USER/g BuildStructure.sh >/tmp/BuildStructure.sh
 cp /tmp/BuildStructure.sh .
+rm /tmp/BuildStructure.sh
 sed s/fftype=fftype/fftype=$analysis/g BuildStructure.sh >/tmp/BuildStructure.sh
 cp /tmp/BuildStructure.sh .
+rm /tmp/BuildStructure
 
 echo "Compiling the framework"
 sh BuildStructure.sh
