ac8027afe4c5e310e3be01a10390292e32c9eaea
Johannes <johannes.brandstetter@cern.ch>
2017-07-20 11:38:09 +0200

    Automatized /afs/hephy.at/user/j/jbrandstetter and user in steerAll

--- ViennaTool/steerAll.sh
+++ ViennaTool/steerAll.sh
@@ -2,7 +2,15 @@
 
 channel=$(grep 'int CHAN' "Settings.h" | awk -F'[=;]' '{print $2}')
 output=$(grep 'output_folder' "Settings.h" | awk -F'[=;]' '{print $2}' | tr -d '"')
+analysis=$(grep 'analysis' "Settings.h" | awk -F'[=;]' '{print $2}' | tr -d '"')
 echo $output
+
+sed s/user=\"whoami\"/user=\"$USER\"/g Settings.h >/tmp/Settings.h
+cp /tmp/Settings.h .
+sed s/user=whoami/user=$USER/g BuildStructure.sh >/tmp/BuildStructure.sh
+cp /tmp/BuildStructure.sh .
+sed s/fftype=fftype/fftype=$analysis/g BuildStructure.sh >/tmp/BuildStructure.sh
+cp /tmp/BuildStructure.sh .
 
 echo "Compiling the framework"
 sh BuildStructure.sh
