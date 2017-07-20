c99a66774b0c16aab84adc90bea100951885e8f2
Johannes <johannes.brandstetter@cern.ch>
2017-07-20 14:23:59 +0200

    answering yes to all syst questions

--- ViennaTool/steerAll.sh
+++ ViennaTool/steerAll.sh
@@ -6,14 +6,11 @@ analysis=$(grep 'analysis' "Settings.h" | awk -F'[=;]' '{print $2}' | tr -d '"')
 echo $HOME$output
 
 sed s/user=\"whoami\"/user=\"$USER\"/g Settings.h >/tmp/Settings$USER.h
-yes|cp -ruv cp /tmp/Settings$USER.h Settings.h
-yes|cp -ruv rm /tmp/Settings$USER.h
+yes | mv /tmp/Settings$USER.h Settings.h
 sed s/user=whoami/user=$USER/g BuildStructure.sh >/tmp/BuildStructure$USER.sh
-yes|cp -ruv cp /tmp/BuildStructure$USER.sh .
-yes|cp -ruv rm /tmp/BuildStructure$USER.sh
+yes | mv /tmp/BuildStructure$USER.sh Buildstructure.sh
 sed s/fftype=fftype/fftype=$analysis/g BuildStructure.sh >/tmp/BuildStructure$USER.sh
-yes|cp -ruv cp /tmp/BuildStructure$USER.sh .
-yes|cp -ruv rm /tmp/BuildStructure$USER.sh
+yes | mv /tmp/BuildStructure$USER.sh Buildstructure.sh
 
 echo "Compiling the framework"
 sh BuildStructure.sh
