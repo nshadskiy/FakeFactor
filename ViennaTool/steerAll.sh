7c505163d9708fc7c47f1c9fb03d7829e605ddab
Johannes <johannes.brandstetter@cern.ch>
2017-07-20 13:57:05 +0200

    user specified tmp files

--- ViennaTool/steerAll.sh
+++ ViennaTool/steerAll.sh
@@ -5,15 +5,15 @@ output=$(grep 'output_folder' "Settings.h" | awk -F'[=;]' '{print $2}' | tr -d '
 analysis=$(grep 'analysis' "Settings.h" | awk -F'[=;]' '{print $2}' | tr -d '"')
 echo $HOME$output
 
-sed s/user=\"whoami\"/user=\"$USER\"/g Settings.h >/tmp/Settings.h
-cp /tmp/Settings.h .
-rm /tmp/Settings.h
-sed s/user=whoami/user=$USER/g BuildStructure.sh >/tmp/BuildStructure.sh
-cp /tmp/BuildStructure.sh .
-rm /tmp/BuildStructure.sh
-sed s/fftype=fftype/fftype=$analysis/g BuildStructure.sh >/tmp/BuildStructure.sh
-cp /tmp/BuildStructure.sh .
-rm /tmp/BuildStructure
+sed s/user=\"whoami\"/user=\"$USER\"/g Settings.h >/tmp/Settings$USER.h
+cp /tmp/Settings$USER.h Settings.h
+rm /tmp/Settings$USER.h
+sed s/user=whoami/user=$USER/g BuildStructure.sh >/tmp/BuildStructure$USER.sh
+cp /tmp/BuildStructure$USER.sh .
+rm /tmp/BuildStructure$USER.sh
+sed s/fftype=fftype/fftype=$analysis/g BuildStructure.sh >/tmp/BuildStructure$USER.sh
+cp /tmp/BuildStructure$USER.sh .
+rm /tmp/BuildStructure$USER.sh
 
 echo "Compiling the framework"
 sh BuildStructure.sh
