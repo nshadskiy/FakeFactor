35385d6948b8d1d586521c6cccaeab96ebc34309
Florian Spreitzer <florian.spreitzer@cern.ch>
2018-12-11 15:54:05 +0100

    Added preselection_EMB

---
+++ ViennaTool/macros/Preselection_EMB.C
@@ -0,0 +1,52 @@
+#include "ViennaTool/interface/TNtupleAnalyzer.h"
+
+#include <iostream>
+#include <string>
+
+void compressFile(TString name);
+
+void Preselection() {
+
+  std::cout << std::endl << "***************************************" << std::endl;
+  std::cout << "*             Preselection            *" << std::endl;
+  std::cout << "***************************************" << std::endl << std::endl;
+
+  std::cout << "Number: " << num << std::endl;
+  TNtupleAnalyzer *Analyzer = new TNtupleAnalyzer();
+  TString tmp;
+
+  if(EMB == 1 && (num==1||num==0)){
+    Analyzer->loadFile(EMBfile,"TauCheck"); 
+    Analyzer->select(preselection_EMB,_DY|_TTAU); //_DY only as placeholder so it gets checked in TNtupleAnalyzer::fitsGenCategory() 
+    Analyzer->closeFile();
+    tmp=preselection_EMB;
+    compressFile(preselection_EMB);
+  }
+    
+  delete Analyzer;
+  
+}
+
+void compressFile(TString fname){
+  TChain* MCch=new TChain("Events");
+
+  TString fname2=fname; fname2.ReplaceAll(".root",".old.root");
+  TString cmd_mv=".!mv "+fname+" "+fname2;
+  gROOT->ProcessLine(cmd_mv);
+
+  MCch->Add(fname2);
+
+  TFile* MCf = new TFile(fname,"recreate");
+  MCch->Merge(MCf,0);
+
+  TString cmd_rm=".!rm -f "+fname2;
+  gROOT->ProcessLine(cmd_rm);
+
+}
+  
+#ifndef __CINT__
+int main(int argc, char* argv[]) {
+  //std::string argument = argc > 1 ? argv[2] : "default";
+  Preselection();    
+}
+#endif
